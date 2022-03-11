f4bb00fe548b3f65356b5984120731539166f7ec
Shivam Kumar Sharma <80200591+shivam248k@users.noreply.github.com>
2022-03-12 02:53:26 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $link = mysqli_connect("localhost", "root","", "shivam24");
+    $link = mysqli_connect("sql112.epizy.com", "epiz_31259133", "2l5roVwhTFbdc", "epiz_31259133_SecretDiary_project");
         
         if (mysqli_connect_error()) {
             
