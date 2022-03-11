fc0e2f3b604358c5427b1085e1ee06c97c146b61
shivam248k <shivamnag248@gmail.com>
2022-03-12 02:18:20 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+  <head>
+        
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title> Secret Diary </title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+      
+      <style type="text/css">
+          
+      .container {
+      
+            text-align: center;
+            width: 400px;
+      }
+          
+      #homePageContainer {
+              
+            margin-top: 190px;
+          
+      }
+          
+          #containerLoggedInPage {
+              
+              margin-top: 70px;
+              
+          }
+          
+          
+      html { 
+          
+          background: url(https://media.istockphoto.com/photos/leatherbound-journal-with-pen-picture-id184092793?b=1&k=20&m=184092793&s=170667a&w=0&h=4HhDq4NlVuhAPCVVSMmfO2vth7GGWLsBWvJkfmMW7Go=) no-repeat center center fixed; 
+          -webkit-background-size: cover;
+          -moz-background-size: cover;
+          -o-background-size: cover;
+          background-size: cover;
+          
+          }
+          
+          body {
+              
+              background: none;
+              color: #FFF;
+              
+          }
+          
+          #logInForm {
+              
+              display:none;
+              
+          }
+          
+          .toggleForms {
+              
+              font-weight: bold;
+              
+          }
+          
+          #diary {
+            height: 660px;
+              width: 1505px;
+          }
+           .cp-text{
+            position: fixed;     
+            text-align: center;    
+            bottom: 15px; 
+            width: 100%;
+            
+                }
+          
+
+              
+          
+          
+      </style>
+  </head>
+  <body>
+
+  
+  <div class="cp-text">Created And Owned By Shivam.</div>
+        
